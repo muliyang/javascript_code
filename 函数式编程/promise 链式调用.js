714f0e86ea25a28d8f364cdd2b0fafc771c6617c
muliyang <muliyang@163.com>
2021-07-10 20:25:44 +0800

    promise

--- 函数式编程/promise 链式调用.js
+++ 函数式编程/promise 链式调用.js
@@ -1,7 +1,7 @@
 // promise 的 链式调用
-const promise = new Promise((resolve,reject)=>{
-	resolve(100)
-})
+const promise = new Promise((resolve, reject) => {
+  resolve(100);
+});
 
 const promise2 = promise.then(
   function onfulfilled() {},
@@ -13,3 +13,23 @@ console.log(promise2);
 
 console.log(promise === promise2);
 // false
+
+promise.then((res) => {
+    return new Promise((resolve, reject) => {
+      resolve("aaa");
+    });
+  })
+  .then((res) => {
+    console.log(res); // 上一个then return 的 返回值 
+    return "foo";
+  })
+  .then((res) => {
+    console.log(res); 
+  })
+  .then((res) => {
+    console.log(res); 
+  });
+
+  // 'aaa'
+  // 'foo'
+  // undefined
